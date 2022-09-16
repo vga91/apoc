aaeb420c9ed25117d2fed2641662f7bfa42c93e0
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-09-16 09:50:18 +0200

    test remote

--- common/src/main/java/apoc/ApocConfig.java
+++ common/src/main/java/apoc/ApocConfig.java
@@ -45,6 +45,7 @@ import static org.neo4j.configuration.GraphDatabaseSettings.plugin_dir;
 import static org.neo4j.configuration.GraphDatabaseSettings.transaction_logs_root_path;
 
 public class ApocConfig extends LifecycleAdapter {
+    // test to remove
     protected static final String SUN_JAVA_COMMAND = "sun.java.command";
     public static final String APOC_IMPORT_FILE_ENABLED = "apoc.import.file.enabled";
     public static final String APOC_EXPORT_FILE_ENABLED = "apoc.export.file.enabled";
