de29fc8250aa8d6e9404c270ffbee1b9dd95dfea
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-11-28 07:58:21 +0100

    removed unused code

--- core/src/main/java/apoc/export/csv/CsvFormat.java
+++ core/src/main/java/apoc/export/csv/CsvFormat.java
@@ -17,7 +17,6 @@ import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.procedure.TerminationGuard;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -259,7 +258,6 @@ public class CsvFormat implements Format {
             } else {
                 csvWriter.writeNext(headerNode.toArray(new String[headerNode.size()]), false);
             }
-
             rows.forEach(row -> csvWriter.writeNext(row.toArray(new String[row.size()]), false));
         } catch (IOException e) {
             throw new RuntimeException(e);
